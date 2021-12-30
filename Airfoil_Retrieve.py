1842a07cf42acb514d17abd5ada807e08de36c6a
Ccruz1000 <Ccruz1000@hotmail.com>
2021-12-30 17:36:48 -0400

    Can succesfully find airfoil coordinate data and save to file

--- Airfoil_Retrieve.py
+++ Airfoil_Retrieve.py
@@ -1,27 +1,46 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
+# Base URL
+Base_URL = 'http://airfoiltools.com'
 # Retrieve airfoiltools html data
 page = requests.get('http://airfoiltools.com/search/airfoils')
-soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, 'lxml')
 
 # Retrieve all hyperlinks from html page, and then remove all that aren't for an airfoil
-links_hold =[]
+links_placehold =[]
 for link in soup.find_all('a'):
-    links_hold.append(link.get('href'))
+    links_placehold.append(link.get('href'))
 links = []
 # String to select only airfoil links
 approved = '/airfoil/details'
-for airfoil in links_hold:
+for airfoil in links_placehold:
     if approved in airfoil:
         links.append(airfoil)
-print(links[1255])
-chosen = input('Input your airfoil:\n')
-chosen = chosen.lower().replace(' ', '')
-print(chosen)
-airfoil_name = []
-for airfoil in links:
-    if chosen in airfoil.lower():
-        airfoil_name.append(airfoil)
+# Enter your airfoils as strings        
+chosen = ['fx76mp140', 'naca2421', 'e420']
+airfoil_name = [] 
+# Allows user to enter airfoil name with capitals and spaces
+for choice in chosen:
+    choice = choice.lower().replace(' ', '')
+    for airfoil in links:
+        if choice in airfoil.lower().replace(' ', ''):
+            airfoil_name.append(airfoil)
+# Retrieve data for each airfoil
+for airfoil in airfoil_name:
+    URL = Base_URL + airfoil
+    airfoil_page = requests.get(URL)
+    soup2 = BeautifulSoup(airfoil_page.content, 'lxml')
+    # Find XFOIL data for Re = 1 million for airfoil
+    airfoil_links = []
+    dat_link = []
+    dat = soup2.find('td', class_='cell2')
+    airfoil_data = open(airfoil.partition('=')[2] + ".txt", "w")
+    n = airfoil_data.write(dat.text.strip())
+    airfoil_data.close()
+    print(dat.text)
+        
 
-print(airfoil_name)
+        # Save .dat link
+        # Link for airfoil data found at line 383
